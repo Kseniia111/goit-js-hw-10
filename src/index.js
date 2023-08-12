97b1ffd6765382b5e70dd783b058c9476e44e47e
Kseniia111 <kseniya.markovych@gmail.com>
2023-08-12 19:03:31 +0300

    Index.js changes

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import { fetchCatByBreed } from './js/cat-api';
+
+import Notiflix from 'notiflix';
+
+const breedSelect = document.querySelector('.breed-seect');
+const catPicture = document.querySelector('.cat-picture');
+const catDescription = document.querySelector('.cat-description');
+const dataLoader = document.querySelector('.loader');
+
+breedSelect.addEventListener('.change', onChangeSelect)
+
+fetchAndRenderBreeds();
+
+function onChangeSelect(event) {
+    dataLoader.classList.remove('invisible');
+    catPicture.innerHTML = '';
+    catDescription.innerHTML = '';
+    const breedId = event.target.value;
+    console.log('breedId', breedId);
+
+    fetchCatByBreed(breedId)
+        .then(breed => renderBreedDescription(breed))
+        .catch(error => {
+            console.error(error);
+            Notiflix.Notify.failure(
+                'Oops! Something went wrong! Try reloading the page!'
+            );
+        })
+        .finally(() => dataLoader.classList.add('invisible'));
+}
+
+export { breedSelect, catPicture, catDescription, dataLoader };
